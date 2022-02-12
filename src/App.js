c926da2376dad319166f1de43bbcfd3cd7c8953d
antonio <ant.consales@gmail.com>
2022-02-12 22:14:17 +0100

    title on video

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Wall from "./assets/videos/wall.mp4";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div class="container">
-          <h1 className="neonText">HAPPY</h1>
-          <h2 className="neonText">HACKING</h2>
-          <img src={logo} className="App-logo" alt="logo" />
-        </div>
-      </header>
-    </div>
+    <>
+      <video autoPlay muted loop className="myVideo">
+        <source src={Wall} type="video/mp4" />
+      </video>
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <h1 className="neonText">HAPPY</h1>
+            <h2 className="neonText">HACKING</h2>
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
